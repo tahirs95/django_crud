a4c6d57ce98d4caf83dac1a1bc493f9f0ea83390
tahirs95 <tahirs95@hotmail.com>
2019-07-31 14:40:19 +0500

    Updated urls.py.

--- sampleproject/urls.py
+++ sampleproject/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(openapi.Info(
     default_version = 'v1',
     description = 'Simple API',
     terms_of_service = "https://www.google.com/policies/terms/",
-    contact = openapi.Contact(email="ali@gmail.com"),
+    contact = openapi.Contact(email="tahirs95@hotmail.com"),
     license = openapi.License(name="BSD License"),
     ),
     public = True,
